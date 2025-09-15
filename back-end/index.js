cb586b2eeef403eba3fa4289663e16b4493980f7
Phuc <bkphuc2003@bkphuc2003gmail.com>
2025-09-15 23:46:02 +0700

    UserUser

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,46 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+// index.js
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import sequelize from "./src/config/dbsetup.js";
+
+// Model
+import User from "./src/models/User.js";
+
+//routes
+import UserRoutes from "./src/routes/UserRoutes.js";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
 
+// Test route
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.json({ data: "API is running..." });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+// Route
+app.use("/api/users", UserRoutes);
+
+// Tạo bảng và chạy server
+(async () => {
+  try {
+    await sequelize.sync(); // tạo bảng nếu chưa có
+    
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Lỗi khởi động server:", error);
+  }
+})();
+
